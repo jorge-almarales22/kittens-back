ba36da62270545dc717e75fa9f92abfda7302122
Jorge Almarales <jorgealmarales22@gmail.com>
2023-10-04 15:37:08 -0500

    fix (Routes) Eliminando ruta de ibtener por id

--- src/controllers/kittens.controller.js
+++ src/controllers/kittens.controller.js
@@ -6,11 +6,3 @@ export const getKittens = (req, res) => {
     res.status(200).json({kittens: cats});
 }
 
-export const getKittenById = (req, res) => {
-    const { id } = req.params;
-
-    const cat = cats.find(cat => cat.id == id);
-
-    res.status(200).json(cat);
-
-}
--- src/routes/kittens.route.js
+++ src/routes/kittens.route.js
@@ -1,7 +1,6 @@
 import {Router} from "express"
-import { getKittenById, getKittens } from "../controllers/kittens.controller.js";
+import { getKittens } from "../controllers/kittens.controller.js";
 
 export const routerKittens = Router();
 
 routerKittens.get('/', getKittens)
-routerKittens.get('/:id', getKittenById)
